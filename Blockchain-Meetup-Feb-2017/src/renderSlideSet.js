736397c0eae1ff1bdd01e0009933f66b2784e780
Marcus R. Brown <me@igetgam.es>
2017-02-21 14:52:36 -0700

    Add a utility to render slides in a SlideSet.

---
+++ Blockchain-Meetup-Feb-2017/src/renderSlideSet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SlideSet } from '@igetgames/spectacle';
+
+export default function renderSlideSet(slideMap, renderer) {
+  return () => {
+    const slides = Object.keys(slideMap).map((slide) => renderer(slide, slideMap[slide]));
+
+    return (
+      <SlideSet>
+        {slides}
+      </SlideSet>
+    );
+  }
+}
