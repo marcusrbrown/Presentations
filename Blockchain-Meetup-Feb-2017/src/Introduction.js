aa4a9a79a4834abee5745802d7c9d08fc0f6accc
Marcus R. Brown <me@igetgam.es>
2017-02-21 15:51:06 -0700

    Add rough draft of Introduction section.

--- Blockchain-Meetup-Feb-2017/src/Introduction.js
+++ Blockchain-Meetup-Feb-2017/src/Introduction.js
@@ -1,27 +1,80 @@
 import React from 'react';
 import {
+  Appear,
   Heading,
+  List,
+  ListItem,
   Slide
 } from '@igetgames/spectacle';
 import renderSlideSet from './renderSlideSet';
 
 const introSlides = {
-  'title': `
+  'bio': `
     <ul>
       <li>Who am I?</li>
       <li>Why am I here?</li>
+    </ul>
+  `,
+  'purpose': `
+    <ul>
       <li>What is the goal?</li>
+    </ul>
+  `,
+  'methods': `
+    <ul>
       <li>How do we achieve that goal?</li>
     </ul>
-  `
+  `,
 };
 
 const renderSlide = (slide, notes) => {
   switch (slide) {
     case 'title':
       return (
-        <Slide key={slide} notes={notes}>
+        <Slide key={slide} notes={notes} transition={["slide"]}>
           <Heading>Introduction</Heading>
+        </Slide>
+      );
+
+    case 'bio':
+      return (
+        <Slide key={slide} notes={notes}>
+          <Heading textColor="tertiary">Introduction</Heading>
+          <Heading size={6}>Bio</Heading>
+          <List>
+            <Appear><ListItem>Hi! I'm Marcus.</ListItem></Appear>
+            <Appear><ListItem>I make games for a living, but freelance full stack development.</ListItem></Appear>
+            <Appear><ListItem>I became interested in bulding Ethereum apps in Spring 2016.</ListItem></Appear>
+            <Appear><ListItem>I volunteered as a Ethereum Classic core developer after the DAO Fork.</ListItem></Appear>
+          </List>
+        </Slide>
+      );
+
+    case 'purpose':
+      return (
+        <Slide key={slide} notes={notes}>
+          <Heading textColor="tertiary">Introduction</Heading>
+          <Heading size={6}>Purpose</Heading>
+          <List>
+            <Appear><ListItem>Rapid prototyping of Ethereum applications, aka "dApps".</ListItem></Appear>
+            <Appear><ListItem>Smart Contract code can be easily developed and tested.</ListItem></Appear>
+            <Appear><ListItem>Easy method of building frontend code that utilizes Smart Contracts.</ListItem></Appear>
+            <Appear><ListItem>Simple deployment to Testnet and production Ethereum blockchains.</ListItem></Appear>
+          </List>
+        </Slide>
+      );
+
+    case 'methods':
+      return (
+        <Slide key={slide} notes={notes}>
+          <Heading textColor="tertiary">Introduction</Heading>
+          <Heading size={6}>Methods</Heading>
+          <List>
+            <Appear><ListItem>Docker to "not care" about low-level setup and configuration.</ListItem></Appear>
+            <Appear><ListItem>Parity for a fast, flexible development blockchain.</ListItem></Appear>
+            <Appear><ListItem>Truffle for Solidity-based Smart Contract development, testing, and deployment.</ListItem></Appear>
+            <Appear><ListItem>Create React App, Web3, and Ethereum JS tools and libraries to build the frontend and interact with the blockchain.</ListItem></Appear>
+          </List>
         </Slide>
       );
 
